e9cf885c7a5ceea7f8735d10c7d55086d4534854
Paul Piilonen <paul@ajdcomputer.com>
2021-05-31 18:20:16 +0000

    Added functions to explain difference between console.log and return

--- hello.js
+++ hello.js
@@ -5,3 +5,19 @@ const sayHello = function(name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// console.log - prints immediately
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+// return - no output without further instructions
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello("Paul");
+
+// add output statement for returned value
+console.log(greeting);
