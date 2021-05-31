6777e8cbc1fa9ce3a66c939d43da24ddaeeec3e0
Paul Piilonen <paul@ajdcomputer.com>
2021-05-31 18:32:31 +0000

    Create function to return boolean result testing for even and odd

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven = function(num) {
+  return (num % 2 === 0);
+}
+
+// Version 1
+/*const tenIsEven = isEven(10);
+const elevenIsOdd = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsOdd);*/
+
+// Version 2
+console.log(isEven(10));
+console.log(isEven(11));
